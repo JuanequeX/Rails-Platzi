a6aa1dc4cc337a4d000eb989d9f819cdc80f5314
Juanequex <ramon.g.uz37@gmail.com>
2022-01-26 12:35:43 -0600

    Second part of the course

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def hello
+    @credentials = Rails.application.credentials.hello
+    pet = Pet.first
+    @name = pet.name
+    @breed = pet.breed
+  end
+end
